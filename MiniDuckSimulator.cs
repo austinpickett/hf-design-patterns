56caee4cad5cc015c83981e741c9edc0ee7081e4
Austin Pickett <pickett.austin@gmail.com>
2016-01-07 22:57:40 -0500

    added dynamic set fly behavior

--- MiniDuckSimulator.cs
+++ MiniDuckSimulator.cs
@@ -4,7 +4,10 @@ namespace Duck {
     public class MiniDuckSimulator {
         public static void Main(string[] args) {
             Duck mallard = new MallardDuck(new FlyWithWings(), new Quack());
+            
             mallard.PerformQuack();
+            mallard.PerformFly();
+            mallard.flyBehavior = new FlyNoWay();
             mallard.PerformFly();
             
             Duck model = new ModelDuck(new FlyWithWings(), new Quack());
